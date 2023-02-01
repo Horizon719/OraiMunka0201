10e11fb71f9a0a503669afe8599ac87068874a7e
Horizon719 <milichovszky.sandor@gmail.com>
2023-02-01 12:16:14 +0100

    vesztés visszaadása

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #megoldas
-def eredmeny():
-    pass
+def eredmeny(jPont, gPont):
+    if jPont > 21:
+        return "Játékos vesztett"
+    elif gPont > 21:
+        return "Gép vesztett"
 #teszt esetek
