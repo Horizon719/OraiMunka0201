092aa2689ae6a35a6fb6d8cc66e4fab16120a0e5
Horizon719 <milichovszky.sandor@gmail.com>
2023-02-01 13:22:20 +0100

    többféle teszt lehetőség

--- main.py
+++ main.py
@@ -17,14 +17,41 @@ def osszegzes(lista: list[int]):
 
 
 def jatekos_vesztett_teszt():
-    jLapok = [10, 5, 6, 8]
-    gLapok = [2, 7]
+    jLapok = [10, 5, 7]
+    gLapok = [2, 7, 9]
+    kapott = eredmeny(jLapok, gLapok)
+    vart = "Játékos vesztett"
+    if kapott == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt megbukott")
 
-    print(eredmeny(jLapok, gLapok))
 
+def gep_vesztett_teszt():
+    jLapok = [10, 5, 4]
+    gLapok = [6, 7, 10]
+    kapott = eredmeny(jLapok, gLapok)
+    vart = "Gép vesztett"
+    if kapott == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt megbukott")
+
+
+
+def dontetlen_teszt():
+    jLapok = [10, 5, 7]
+    gLapok = [6, 7, 9]
+
+    if osszegzes(jLapok) == osszegzes(gLapok):
+        print("A teszt sikeres")
+    else:
+        print("A teszt megbukott")
 
 def tesztek():
     jatekos_vesztett_teszt()
+    gep_vesztett_teszt()
+    dontetlen_teszt()
 
 
 tesztek()
