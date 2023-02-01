e15a9625fcbcc4f5919d9b0ba969942ac5d932de
Horizon719 <milichovszky.sandor@gmail.com>
2023-02-01 12:30:18 +0100

    összegzés függvény és lista

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #megoldas
 def eredmeny(jPont, gPont):
-    if jPont > 21:
+    if osszegzes(jPont) > 21:
         return "Játékos vesztett"
-    elif gPont > 21:
+    elif osszegzes(gPont) > 21:
         return "Gép vesztett"
+
+def osszegzes(lista):
+    i = 0
+    osszeg = 0
+    while i < len(lista):
+        osszeg += lista[i]
+        i += 1
+    return osszeg
 #teszt esetek
