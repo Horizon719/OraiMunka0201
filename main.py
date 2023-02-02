d2d0b54f1a94b83bd6fcc5060df5390184516a08
Horizon719 <milichovszky.sandor@gmail.com>
2023-02-02 13:12:44 +0100

    9 teszteset

--- main.py
+++ main.py
@@ -22,9 +22,31 @@ def jatekos_vesztett_teszt():
     kapott = eredmeny(jLapok, gLapok)
     vart = "Játékos vesztett"
     if kapott == vart:
-        print("A teszt sikeres")
+        print("A játékos_vesztett_teszt sikeres")
     else:
-        print("A teszt megbukott")
+        print("A játékos_vesztett_teszt megbukott")
+
+
+def jatekos_vesztett_teszt2():
+    jLapok = [10, 1, 7]
+    gLapok = [5, 7, 9]
+    kapott = eredmeny(jLapok, gLapok)
+    vart = "Játékos vesztett"
+    if kapott == vart:
+        print("A játékos_vesztett_teszt2 sikeres")
+    else:
+        print("A játékos_vesztett_teszt2 megbukott")
+
+
+def jatekos_vesztett_teszt3():
+    jLapok = [8, 5, 6]
+    gLapok = [10, 11]
+    kapott = eredmeny(jLapok, gLapok)
+    vart = "Játékos vesztett"
+    if kapott == vart:
+        print("A játékos_vesztett_teszt3 sikeres")
+    else:
+        print("A játékos_vesztett_teszt3 megbukott")
 
 
 def gep_vesztett_teszt():
@@ -33,25 +55,76 @@ def gep_vesztett_teszt():
     kapott = eredmeny(jLapok, gLapok)
     vart = "Gép vesztett"
     if kapott == vart:
-        print("A teszt sikeres")
+        print("A gép_vesztett_teszt sikeres")
     else:
-        print("A teszt megbukott")
+        print("A gép_vesztett_teszt megbukott")
 
+
+def gep_vesztett_teszt2():
+    jLapok = [9, 1, 7]
+    gLapok = [5, 7, 10]
+    kapott = eredmeny(jLapok, gLapok)
+    vart = "Gép vesztett"
+    if kapott == vart:
+        print("A gép_vesztett_teszt2 sikeres")
+    else:
+        print("A gép_vesztett_teszt2 megbukott")
+
+
+def gep_vesztett_teszt3():
+    jLapok = [10, 11]
+    gLapok = [8, 5, 6]
+    kapott = eredmeny(jLapok, gLapok)
+    vart = "Gép vesztett"
+    if kapott == vart:
+        print("A gép_vesztett_teszt3 sikeres")
+    else:
+        print("A gép_vesztett_teszt3 megbukott")
 
 
 def dontetlen_teszt():
     jLapok = [10, 5, 7]
-    gLapok = [6, 7, 9]
-
-    if osszegzes(jLapok) == osszegzes(gLapok):
-        print("A teszt sikeres")
+    gLapok = [2, 7, 9]
+    kapott = eredmeny(jLapok, gLapok)
+    vart = "Döntetlen vesztett"
+    if kapott == vart:
+        print("A döntetlen_teszt sikeres")
     else:
-        print("A teszt megbukott")
+        print("A döntetlen_teszt megbukott")
+
+
+def dontetlen_teszt2():
+    jLapok = [5, 6, 10]
+    gLapok = [11, 4, 6]
+    kapott = eredmeny(jLapok, gLapok)
+    vart = "Döntetlen vesztett"
+    if kapott == vart:
+        print("A döntetlen_teszt2 sikeres")
+    else:
+        print("A döntetlen_teszt2 megbukott")
+
+
+def dontetlen_teszt3():
+    jLapok = [8, 9]
+    gLapok = [10, 7]
+    kapott = eredmeny(jLapok, gLapok)
+    vart = "Döntetlen vesztett"
+    if kapott == vart:
+        print("A döntetlen_teszt3 sikeres")
+    else:
+        print("A döntetlen_teszt3 megbukott")
+
 
 def tesztek():
     jatekos_vesztett_teszt()
+    jatekos_vesztett_teszt2()
+    jatekos_vesztett_teszt3()
     gep_vesztett_teszt()
+    gep_vesztett_teszt2()
+    gep_vesztett_teszt3()
     dontetlen_teszt()
+    dontetlen_teszt2()
+    dontetlen_teszt3()
 
 
 tesztek()
